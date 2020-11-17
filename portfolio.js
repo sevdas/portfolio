89fd39cbae97fa6986e2b561c8d3c11c765b34b7
Sevda Shukrieva <sshukrieva@gmail.com>
2020-11-17 23:35:17 +0000

    add changing text to the talking-bubble, change background color

--- portfolio.js
+++ portfolio.js
@@ -3,8 +3,11 @@ const happyFace = document.querySelector("#face-happy")
 const sadFace = document.querySelector("#face-sad")
 const helloMsg = document.querySelector("#hello-message")
 
-sadFace.style.display='none'
+const homeSection = document.querySelector('#home')
+const bubbleMessage = document.querySelector('#talkbubble-message')
 
+
+sadFace.style.display='none'
 let count = 0
 
 function moodyFace() {
@@ -12,12 +15,19 @@ function moodyFace() {
    if (count%2 === 0) {
     happyFace.style.display='block'
     sadFace.style.display='none'
+
        helloMsg.textContent = 'Hello! Welcome to my world!'
+       helloMsg.style.color = 'black'
+       homeSection.style.background = '#f4f6ef'
+       bubbleMessage.textContent = "Now you know! Scroll down!" 
+
    } else {
        happyFace.style.display='none'
        sadFace.style.display='block'
+       homeSection.style.background  = 'black' 
+       bubbleMessage.textContent = 'I can not see you! :/ Switch back please!'
        helloMsg.textContent = 'Uh! Panic!'
- 
+       helloMsg.style.color = 'white'
  }
 }
 
