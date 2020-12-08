7efad93d8f8ae39a81fde2cb17ac6790bfa4b045
Sevda Shukrieva <sshukrieva@gmail.com>
2020-12-08 12:34:48 +0000

    add logic for carousel next and previous click event, fix css

--- index.js
+++ index.js
@@ -60,101 +60,42 @@ happyFace.addEventListener('click', moodyFace)
 sadFace.addEventListener('click', moodyFace)
 
 
-
-
 //CAROUSEL
 const trackList = document.querySelector('.carousel-track-list')
-const carouselSlides = Array.from(trackList.children)
+const carouselSlides = document.querySelectorAll('.carousel-slide')
 const nextButton = document.querySelector('.carousel-button--next')
 const prevButton = document.querySelector('.carousel-button--prev')
 
 
-//Define size of an element and its position relative to the viewport.
-const slideSize = carouselSlides[0].getBoundingClientRect()
-const sizeWidth = slideSize.width
+let currentIndex = 0
 
-
-//Arrange slides next to one another 
-const leftSlidePosition = carouselSlides.forEach((slide, idx) => slide.style.left = `${sizeWidth * idx}px`)
-
-
-function moveToSlidePosition(trackList, currentSlide, targetSlide){
-  //Move an element sideways to the next one by that much
-  trackList.style.transform = 'translateX(-'+ targetSlide.style.left +')'
-  
-  //Add current position class to the every next slide
-   currentSlide.classList.remove('current-slide')
-   targetSlide.classList.add('current-slide')
+//Check if 
+const checkLimits = () => {
+  if(currentIndex >  carouselSlides.length-1){
+    currentIndex = 0
+  } else if(currentIndex < 0 ){
+    currentIndex = carouselSlides.length-1
   }
-
-//Show current image
-  function showWhichImage(currentThumbImage, targetImage){
-    currentThumbImage.classList.remove('current-thumb-slide')
-    targetImage.classList.add('current-thumb-slide')
-  }
-
-
-  // Display or hide carousel arrow when all the way to the left or right. 
-  prevButton.style.display = 'none'
-  function displayOnOffArrowButton(targetIndex, nextButton, prevButton, carouselSlides){
-  if(targetIndex === 0){
-    prevButton.style.display = 'none'
-    nextButton.style.display = 'block'
-  } else if (targetIndex === carouselSlides.length - 1) {
-    nextButton.style.display = 'none'
-    prevButton.style.display = 'block'
-  } else {
-    prevButton.style.display = 'block' 
-    nextButton.style.display = 'block'
-  }
-  }
-
+}
 
 //When I click next button, move slides to the right. 
-nextButton.addEventListener('click', event => {
-  const currentSlide = trackList.querySelector('.current-slide')
-  const nextSlide = currentSlide.nextElementSibling
-  const nextIndex = carouselSlides.findIndex(slide => slide === nextSlide)
-  const currentThumbImage = document.querySelector('.current-thumb-slide')
-  const nextImage = currentThumbImage.nextElementSibling
-
-  moveToSlidePosition(trackList, currentSlide, nextSlide)
-  displayOnOffArrowButton(nextIndex, nextButton, prevButton, carouselSlides)
-  showWhichImage(currentThumbImage, nextImage)
-})
+nextButton.addEventListener('click', function nextImage() { 
+carouselSlides[currentIndex].classList.remove('visible')
+carouselSlides[currentIndex].classList.add('hidden')
+currentIndex++
+checkLimits()
+carouselSlides[currentIndex].classList.add('visible')
+carouselSlides[currentIndex].classList.remove('hidden')
+console.log(currentIndex)
+})  
 
 
 //When I click previous button, move slides to the left. 
-prevButton.addEventListener('click', event => {
-  const currentSlide = trackList.querySelector('.current-slide')
-  const prevSlide = currentSlide.previousElementSibling
-  const prevIndex = carouselSlides.findIndex(slide => slide === prevSlide)
-  const currentThumbImage = document.querySelector('.current-thumb-slide')
-  const prevImage = currentThumbImage.previousElementSibling
-console.log('test', prevImage)
-  moveToSlidePosition(trackList, currentSlide, prevSlide)
-  displayOnOffArrowButton(prevIndex, nextButton, prevButton, carouselSlides)
-  showWhichImage(currentThumbImage, prevImage)
+prevButton.addEventListener('click', function() {
+carouselSlides[currentIndex].classList.remove('visible')
+carouselSlides[currentIndex].classList.add('hidden')
+currentIndex--
+checkLimits()
+carouselSlides[currentIndex].classList.add('visible')
+carouselSlides[currentIndex].classList.remove('hidden')
 })
-
-
-//When I click to the thumbnail image gallery, move to the slide. 
-  const thumbnailTrack = document.querySelector('.thumbnail-track-list')
-  const thumbImages = document.querySelectorAll('.thumbnail-image')
-  const arrayThumbImages = Array.from(thumbImages)
-  const currentThumbImage = document.querySelector('.current-thumb-slide')
-
-function moveToThumbnail(event){ 
-  const targetImage = event.target //image on click
-  const targetIndex = arrayThumbImages.findIndex(img => img === targetImage) //which image was clicked on out of array
-  const targetSlide = carouselSlides[targetIndex] //navigate in between the thumb images 
-  const currentSlide = trackList.querySelector('.current-slide')
-  const currentThumbImage = document.querySelector('.current-thumb-slide')
-
-  moveToSlidePosition(trackList, currentSlide, targetSlide)
-  displayOnOffArrowButton(targetIndex, nextButton, prevButton, carouselSlides)
-  showWhichImage(currentThumbImage, targetImage)
-
-}
-thumbnailTrack.addEventListener('click', moveToThumbnail)
-
