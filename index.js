6b5e4ee306b5141314eda83a0ccbdbb90db9b3ca
Sevda Shukrieva <sshukrieva@gmail.com>
2020-12-09 20:34:13 +0000

    add event click to the thumbnail imagery, set up reset styling

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ typeWriterWelcome()
 
 //MOODY FACE
 sadFace.style.display='none'
-let count = 0
+let count = 0 //Start point
 
 function moodyFace() {
    count++
@@ -67,18 +67,20 @@ const nextButton = document.querySelector('.carousel-button--next')
 const prevButton = document.querySelector('.carousel-button--prev')
 
 
-let currentIndex = 0
+let currentIndex = 0 // Start point
 
-//Check if 
+//Check 
 const checkLimits = () => {
+      // to prevent larger values than the slide length
   if(currentIndex > carouselSlides.length-1){
     currentIndex = 0
+     // to avoide negative values
   } else if(currentIndex < 0 ){
     currentIndex = carouselSlides.length-1
   }
 }
 
-//When I click next button, move slides to the right. 
+//When user clicked next button, move slides to the right. 
 nextButton.addEventListener('click', function nextImage() { 
 carouselSlides[currentIndex].classList.remove('visible')
 carouselSlides[currentIndex].classList.add('hidden')
@@ -87,21 +89,21 @@ checkLimits()
 carouselSlides[currentIndex].classList.add('visible')
 carouselSlides[currentIndex].classList.remove('hidden')
 displayOnOffArrowButton(currentIndex)
-console.log(currentIndex)
 })  
 
 
 //When I click previous button, move slides to the left. 
-prevButton.addEventListener('click', function() {
-carouselSlides[currentIndex].classList.remove('visible')
-carouselSlides[currentIndex].classList.add('hidden')
-currentIndex--
-checkLimits()
-carouselSlides[currentIndex].classList.add('visible')
-carouselSlides[currentIndex].classList.remove('hidden')
-displayOnOffArrowButton(currentIndex)
-console.log(currentIndex)
-})
+    prevButton.addEventListener('click', function prevImage() { 
+    carouselSlides[currentIndex].classList.remove('visible')
+    carouselSlides[currentIndex].classList.add('hidden')
+    currentIndex--
+    checkLimits()
+    carouselSlides[currentIndex].classList.add('visible')
+    carouselSlides[currentIndex].classList.remove('hidden')
+    displayOnOffArrowButton(currentIndex)
+  })
+  
+
 
 // Display or hide carousel arrow button when all the way to the left or right. 
 function displayOnOffArrowButton(currentIndex){
@@ -116,3 +118,22 @@ function displayOnOffArrowButton(currentIndex){
   nextButton.classList.remove('hidden')
  }
 }
+
+//Thumbnail Image Gallery
+const thumbnailTrackList = document.querySelector('.thumbnail-track-list')
+const thumbnailSlideImages = document.querySelectorAll('.thumbnail-slide img')
+
+// Add event listener to each image element
+thumbnailSlideImages.forEach(thumbImg => 
+  thumbImg.addEventListener('click',function (event){
+     event.target //event on click 
+
+  //Reset the styling
+  thumbnailSlideImages.forEach(thumbImg => (thumbImg.style.filter = 'opacity(100%)') && (thumbImg.style.borderRadius = '0px'))
+  thumbImg.style.filter = 'opacity(40%)'
+  thumbImg.style.borderRadius = '50px'
+
+}, false))
+
+
+
